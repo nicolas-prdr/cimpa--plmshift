6b77b2310439cc78badc1b96026f7e96a7136aa8
Philippe Depouilly <Philippe.Depouilly@math.u-bordeaux.fr>
2021-12-17 17:27:28 +0100

    move sites in web dir

--- sites/default/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (getenv('OPENSHIFT_BUILD_NAME')) {
-  if (file_exists($app_root . '/' . $site_path . '/settings.openshift.php')) {
-    include $app_root . '/' . $site_path . '/settings.openshift.php';
-  }
-}
-
