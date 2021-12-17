705476c001807e9054690752629c3d10bc901bb8
Philippe Depouilly <Philippe.Depouilly@math.u-bordeaux.fr>
2021-12-17 17:35:07 +0100

    move sites in web dir

--- php-pre-start/drupal-sync.sh
+++ php-pre-start/drupal-sync.sh
@@ -2,13 +2,17 @@
 
 set -e
 
-if [ ! -f sites/default/settings.php ];then
+if [ ! -f website/web/sites/default/settings.php ];then
   echo "Sync la distribution Drupal dans le dossier website, merci de patienter..."
-  rsync -av /opt/app-root/drupal-dist/ .
-  fix-permissions ./
+  rsync -a /opt/app-root/drupal-dist/ website/
 fi
 
+echo "Sync de la customisation su site"
+rsync -av web website/
+fix-permissions ./
+
 # Apres une installation reussie, on bloque l'acces en ecriture a settings.php
-if [ -d sites/default/files/languages ] && [ -w sites/default/settings.php ]; then
+if [ -d website/web/sites/default/files/languages ] && [ -w website/web/sites/default/settings.php ]; then
   chmod 440 website/drupal/web/sites/default/settings.php
 fi
+
